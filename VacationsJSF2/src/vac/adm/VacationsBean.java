7f7bd10fec1fc63bb2ac51b5273d27379776ff19
maksim_nikitin <nikitinmv1987@gmail.com>
2013-10-21 15:02:13 +0300

    [*] вкладка "Відгули"

--- VacationsJSF2/src/vac/adm/VacationsBean.java
+++ VacationsJSF2/src/vac/adm/VacationsBean.java
@@ -28,7 +28,79 @@ public class VacationsBean implements Serializable{
 	private HtmlDataTable dataTableAccruals;
 
 	@Inject
-	private EmployeesBean employeesBean;	
+	private EmployeesBean employeesBean;
+	
+	public double getSum() throws SQLException {
+		Connection conn = ds.getConnection();
+		double result;
+		try {
+			PreparedStatement stmt = conn.prepareStatement(
+					"Use Vacations " +
+					"SELECT " +
+						"SUM(Volume) BALANCE " +
+					"FROM Vacations " +
+					"WHERE IdEmloyee = ?");
+			
+			stmt.setInt(1, employeesBean.getItemEmployee().getIdEmploees());			
+			ResultSet rs = stmt.executeQuery();
+			
+			rs.next();
+			result = rs.getDouble("BALANCE");
+		}
+		finally
+		{
+			conn.close();
+		}
+		return result;	
+	}
+	
+	public double getSumPos() throws SQLException {
+		Connection conn = ds.getConnection();
+		double result;
+		try {
+			PreparedStatement stmt = conn.prepareStatement(
+					"Use Vacations " +
+					"SELECT " +
+						"SUM(Volume) SUMPOS " +
+					"FROM Vacations " +
+					"WHERE Volume>0 AND IdEmloyee = ?");
+			
+			stmt.setInt(1, employeesBean.getItemEmployee().getIdEmploees());			
+			ResultSet rs = stmt.executeQuery();
+			
+			rs.next();
+			result = rs.getDouble("SUMPOS");
+		}
+		finally
+		{
+			conn.close();
+		}
+		return result;	
+	}
+	
+	public double getSumNeg() throws SQLException {
+		Connection conn = ds.getConnection();
+		double result;
+		try {
+			PreparedStatement stmt = conn.prepareStatement(
+					"Use Vacations " +
+					"SELECT " +
+						"ABS(SUM(Volume)) SUMNEG " +
+					"FROM Vacations " +
+					"WHERE Volume<0 AND IdEmloyee = ?");
+			
+			stmt.setInt(1, employeesBean.getItemEmployee().getIdEmploees());			
+			ResultSet rs = stmt.executeQuery();
+			
+			rs.next();
+			result = rs.getDouble("SUMNEG");
+		}
+		finally
+		{
+			conn.close();
+		}
+		return result;	
+	}
 	
 	public List<Accrual> getAccrualsList() throws SQLException {
 		System.out.println("preparing to get accruals");
