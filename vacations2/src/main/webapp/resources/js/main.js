e90b8ef18081ed6e1d1c6c882641be0c8e3829e3
maksim_nikitin <nikitinmv1987@gmail.com>
2013-11-25 11:32:59 +0200

    [*] дизайн. левую панель превращаем в верхнюю

--- vacations2/src/main/webapp/resources/js/main.js
+++ vacations2/src/main/webapp/resources/js/main.js
@@ -25,5 +25,17 @@ window.onload = function highlightSelectedRow() {
 	        .getElementsByTagName('tr');
 	    var i = document.getElementById("rowIndex").value -1; 
 	    trs[i].bgColor = '#f5f5f5';
-	}
-};
+	};	
+}
+
+$(function(){
+	 var url = window.location.pathname;  
+	 var activePage = url.substring(url.lastIndexOf('/')+1);
+	$('.nav li a').each(function(){  
+	var currentPage = this.href.substring(this.href.lastIndexOf('/')+1);
+
+	if (activePage == currentPage) {
+	$(this).parent().addClass('active'); 
+	} 
+	});
+	 })
