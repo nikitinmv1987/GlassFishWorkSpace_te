02799a5425e30fe82657e94c5715eb354f4da5c1
maksim_nikitin <nikitinmv1987@gmail.com>
2013-12-03 16:22:30 +0200

    [*] автодополнение + разметка

--- demo1/src/main/java/bill/PaymentBean.java
+++ demo1/src/main/java/bill/PaymentBean.java
@@ -1,6 +1,8 @@
 package bill;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
@@ -13,11 +15,31 @@ public class PaymentBean implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private String text;
+	private String txt1;
+	
+	List<String> list = new ArrayList<String>();  
 
 	public PaymentBean() {
+		
+		list.add("Предоставление");
+		list.add("Предоставление");
+		list.add("Размещение");
+		list.add("Упоминание");
+		list.add("Возможность размещения");
+		list.add("Использование");
+		list.add("Трансляция");
+		list.add("Приоритетное право");
 			
 	}
 	
+	public List<String> getList() {
+		return list;
+	}
+
+	public void setList() {
+
+	}
+
 	public String createPayment() {		
 		System.out.println("PaymentBean.createPayment()");
 		return "success";		
@@ -43,4 +65,23 @@ public class PaymentBean implements Serializable {
 	public void setText(String text) {
 		this.text = text;
 	}
+
+	public String getTxt1() {
+		return txt1;
+	}
+
+	public void setTxt1(String txt1) {
+		this.txt1 = txt1;
+	}
+	
+	 public List<String> complete(String query) {  
+	        List<String> results = new ArrayList<String>();
+	               
+	        for (String cur : list) {
+				if (cur.toLowerCase().matches(query.toLowerCase() + "(.*)")) {
+					results.add(cur);
+				}
+			}	        
+	        return results;  
+	    }  
 }
--- demo1/src/main/webapp/resources/js/forPayment.js
+++ demo1/src/main/webapp/resources/js/forPayment.js
@@ -2,37 +2,46 @@ shortcut.add("Esc", function() {
 	blockClick(blockReturn);    
 });
 
-shortcut.add("Enter", function() {
-	blockClick(blockSave);
-});
+//shortcut.add("Enter", function() {
+	//blockClick(blockSave);
+//});
 
 //next focus2
-var idj = "summ";
+var idj = "service";
 
 shortcut.add("Tab", function() {		
 	if (idj == "number") {
-		idj = "summ";
+		idj = "sum";
 		document.getElementById("form1:number").focus();
 	}
-	else if (idj == "summ") {
+	else if (idj == "sum") {
 		idj = "blockSave";
-		document.getElementById("form1:summ").focus();
+		document.getElementById("form1:sum").focus();
 	} 
 	else if (idj == "blockSave") {
-		idj = "number";
+		idj = "service_inp";
 		document.getElementById("form1:blockSave").focus();
 	}		
+	else if (idj == "service") {
+		idj = "service";
+		document.getElementById("form1:service").getElementsByTagName("input")[0].focus();
+		
+		//$('#service').next().find('input').focus();
+	}	
 });
 
 function nextfocus(idp) {
+	if (idp=="service") {		
+		idj = "number";
+	}	
 	if (idp=="number") {
-		idj = "summ";
+		idj = "sum";
 	}
-	else if (idp=="summ") {
+	else if (idp=="sum") {
 		idj = "blockSave";
 	}
 	else if (idp=="blockSave") {
-		idj = "number";
+		idj = "service";
 	}
 };
 	
